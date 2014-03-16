97101b657876fb7777ea4e52729c201fef503a82
Ian Macalinao <ian@giza.us>
2014-03-16 03:58:01 -0500

    Add vim to make script
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc"    # list of files/folders to symlink in homedir
+files="vim vimrc zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
