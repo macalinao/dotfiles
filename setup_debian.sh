7915b401248c85ad6b3fb91366f5eddc9607fecc
Ian Macalinao <ian@giza.us>
2014-03-19 20:28:31 -0500

    Install zsh and vim
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- setup_debian.sh
+++ setup_debian.sh
@@ -1,14 +1,14 @@
 # Mandatory stuff
-sudo apt-get install tmux xfce4 xfce4-goodies
+sudo apt-get install zsh vim tmux xfce4 xfce4-goodies
 # Git is already installed -- no need to worry.
 
 read -p "Install social utilities? (y/n): " RESP
 if [ "$RESP" = "y" ]; then
-    sudo apt-get install pidgin xchat
+    sudo apt-get -y install pidgin xchat
 fi
 
 read -p "Install development tools? (y/n): " RESP
 if [ "$RESP" = "y" ]; then
-    sudo apt-get install openjdk-7-jdk openjdk-7-jre 
-    sudo apt-get install python
+    sudo apt-get -y install openjdk-7-jdk openjdk-7-jre 
+    sudo apt-get -y install python
 fi
