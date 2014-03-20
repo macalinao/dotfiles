6184c04575d6434762c625362f4c6d1901dda0a6
Ian Macalinao <ian@giza.us>
2014-03-19 20:53:17 -0500

    Remove sudo stuff
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- setup_debian.sh
+++ setup_debian.sh
@@ -1,17 +1,28 @@
+# Temp directory
+mkdir ~/tmp
+cd ~/tmp
+
 # Mandatory stuff
-sudo apt-get -y install zsh vim tmux xfce4 xfce4-goodies
+apt-get -y install zsh vim tmux xfce4 xfce4-goodies
 # Git is already installed -- no need to worry.
 
 read -p "Install social utilities? (y/n): " RESP
 if [ "$RESP" = "y" ]; then
-    sudo apt-get -y install pidgin xchat chromium chromium-browser
+    apt-get -y install pidgin xchat chromium chromium-browser
 fi
 
 read -p "Install development tools? (y/n): " RESP
 if [ "$RESP" = "y" ]; then
-    sudo apt-get -y install openjdk-7-jdk openjdk-7-jre 
-    sudo apt-get -y install python
+    apt-get -y install openjdk-7-jdk openjdk-7-jre 
+    apt-get -y install python
 
     # GVM - gradle/groovy
     curl -s get.gvmtool.net | sh
+    
+    # Node
+    git clone https://github.com/visionmedia/n.git
+    cd n
+    make install
+    n latest
+    cd ..
 fi
