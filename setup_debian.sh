0f491fcc03b87ecfc912a1161ac594872b660375
Ian Macalinao <ian@giza.us>
2014-03-19 20:35:26 -0500

    GVM
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- setup_debian.sh
+++ setup_debian.sh
@@ -1,5 +1,5 @@
 # Mandatory stuff
-sudo apt-get install zsh vim tmux xfce4 xfce4-goodies
+sudo apt-get -y install zsh vim tmux xfce4 xfce4-goodies
 # Git is already installed -- no need to worry.
 
 read -p "Install social utilities? (y/n): " RESP
@@ -11,4 +11,7 @@ read -p "Install development tools? (y/n): " RESP
 if [ "$RESP" = "y" ]; then
     sudo apt-get -y install openjdk-7-jdk openjdk-7-jre 
     sudo apt-get -y install python
+
+    # GVM - gradle/groovy
+    curl -s get.gvmtool.net | sh
 fi
