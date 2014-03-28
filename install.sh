2629536fd34fabf63124d3ee2930cb95f0f99e28
Ian Macalinao <ian@giza.us>
2014-03-28 07:03:09 -0500

    Fix install script links
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $HOME/.$file $dir/$file/
 done
 
 echo "Installing fonts..."
