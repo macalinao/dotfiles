8a75fb391d0d232d0734df2f8274b96afe3bda5c
Ian Macalinao <me@ian.pw>
2015-12-18 23:30:40 -0600

    style: 2 spaces on install script

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ cd $(dirname $0)/../
 files="fonts gitignore_global tmux.conf vim vimrc oh-my-zsh zshrc"
 
 for file in $files; do
-    echo "Creating symlink for $dir/$file at $HOME/.$file."
-    if [ -L $HOME/.$file ]; then
-        unlink $HOME/.$file
-    fi
-    ln -s $dir/$file $HOME/.$file
+  echo "Creating symlink for $dir/$file at $HOME/.$file."
+  if [ -L $HOME/.$file ]; then
+    unlink $HOME/.$file
+  fi
+  ln -s $dir/$file $HOME/.$file
 done
 
 echo "Setting global Git settings..."
