5f501af73d1219bb0b57e56bc8bc356fe73daed8
Ian Macalinao <me@ian.pw>
2017-01-10 14:34:51 -0600

    ecr getlogin

--- lib/util.sh
+++ lib/util.sh
@@ -9,7 +9,8 @@ mongodb() {
 alias dcos-ssh="dcos node ssh --master-proxy --leader --user=centos"
 alias dcos-cqlsh="dcos-ssh 'sudo docker run -ti cassandra:3.4 cqlsh node-0.cassandra.mesos'"
 
-alias aws-ecrlogin="aws ecr get-login --region=us-west-2 --profile=asuna | bash"
+alias aws-ecrgetlogin="aws ecr get-login --region=us-west-2"
+alias aws-ecrlogin="aws ecr get-login --region=us-west-2 | bash"
 alias dockenv='eval "$(docker-machine env default)"'
 alias emacsd='emacs --daemon'
 alias emacst='emacsclient -t'
