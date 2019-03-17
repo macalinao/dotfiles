c4202fe1a4d47412ed552d9ebde2bcd2d76a6f69
Ian Macalinao <me@ian.pw>
2019-03-17 11:59:09 -0700

    Set up vscode

--- nixhome/home.nix
+++ nixhome/home.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
 {
+  # Sorry RMS, MS is too alluring
+  nixpkgs.config.allowUnfree = true;
+
   home.packages = [
     pkgs.htop
     pkgs.fortune
@@ -14,5 +17,46 @@
 
   programs.home-manager = {
     enable = true;
+  };
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.Nix
+    ]
+    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      # Get these hashes by putting in the wrong hash.
+      # `home-manager switch` will tell you what the correct hash is.
+      {
+        name = "vscode-docker";
+        publisher = "peterjausovec";
+        version = "0.6.0";
+        sha256 = "07gavimf3037vbd2a6753aw7i9966q55f0712pgzgimbw6c4cyf7";
+      }
+      {
+        name = "gitlens";
+        publisher = "eamodio";
+        version = "9.1.0";
+        sha256 = "0a6iqnqmig0s4d107vzwygybndd9hq99kk6mykc88c8qgwf0zdrr";
+      }
+      {
+        name = "vscode-proto3";
+        publisher = "zxh404";
+        version = "0.2.2";
+        sha256 = "1gasx3ay31dy663fcnhpvbys5p7xjvv30skpsqajyi9x2j04akaq";
+      }
+      {
+        name = "vim";
+        publisher = "vscodevim";
+        version = "0.16.14";
+        sha256 = "0b8d3sj3754l3bwcb5cdn2z4z0nv6vj2vvaiyhrjhrc978zw7mby";
+      }
+      {
+        name = "vsc-material-theme";
+        publisher = "equinusocio";
+        version = "2.6.3";
+        sha256 = "1ghqp0yfcpcnjcwgvxw7aix9fj5q8kr0i97lmzlw2jqslmyvxg5x";
+      }
+    ];
   };
 }
