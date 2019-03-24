26cb9dcb55416d69c1a2013258046b5144b10bfa
Ian Macalinao <me@ian.pw>
2019-03-24 00:01:28 -0700

    trust all local

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -136,7 +136,7 @@
     enableTCPIP = true;
     authentication = pkgs.lib.mkOverride 10 ''
       local all all trust
-      host all all ::1/128 trust
+      host all all 127.0.0.1/32 trust
     '';
     initialScript = pkgs.writeText "backend-initScript" ''
       CREATE ROLE abacus WITH LOGIN PASSWORD 'abacus' CREATEDB;
