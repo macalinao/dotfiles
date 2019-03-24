e516671e74d2305f84c7ee0e8b842ec1b02a1589
Ian Macalinao <me@ian.pw>
2019-03-23 23:12:05 -0700

    Spacemacs managed by nixos

--- nixhome/dotfiles.nix
+++ nixhome/dotfiles.nix
@@ -2,6 +2,12 @@
 
 let
   dotfiles = "${config.home.homeDirectory}/dotfiles/dotfiles";
+  spacemacs = builtins.fetchGit {
+    name = "spacemacs";
+    url = "git://github.com/syl20bnr/spacemacs";
+    ref = "develop";
+    rev = "4b195ddfc9228256361e0b264fe974aa86ed51a8";
+  };
 in {
   home.file.".gitconfig".source = "${dotfiles}/gitconfig";
   home.file.".spacemacs".source = "${dotfiles}/spacemacs";
@@ -9,6 +15,11 @@ in {
   home.file.".vimrc".source = "${dotfiles}/vimrc";
   home.file.".vim" = {
     source = "${dotfiles}/vim";
+    recursive = true;
+  };
+
+  home.file.".emacs.d" = {
+    source = spacemacs;
     recursive = true;
   };
 
