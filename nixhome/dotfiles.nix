d6321ab2f25b10da8f51fceb5180faa505c5062b
Ian Macalinao <me@ian.pw>
2019-03-23 23:04:29 -0700

    Migrate dotfile maangement to home-manager

--- nixhome/dotfiles.nix
+++ nixhome/dotfiles.nix
@@ -3,6 +3,15 @@
 let
   dotfiles = "${config.home.homeDirectory}/dotfiles/dotfiles";
 in {
+  home.file.".gitconfig".source = "${dotfiles}/gitconfig";
+  home.file.".spacemacs".source = "${dotfiles}/spacemacs";
+
+  home.file.".vimrc".source = "${dotfiles}/vimrc";
+  home.file.".vim" = {
+    source = "${dotfiles}/vim";
+    recursive = true;
+  };
+
   home.file.".sbt" = {
     source = "${dotfiles}/sbt";
     recursive = true;
