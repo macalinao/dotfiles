0874b711a969b76fda0ad70362694789b15a358c
Ian Macalinao <me@ian.pw>
2019-03-24 22:37:52 -0700

    private-secrets dotfile management

--- nixhome/dotfiles.nix
+++ nixhome/dotfiles.nix
@@ -2,6 +2,8 @@
 
 let
   dotfiles = "${config.home.homeDirectory}/dotfiles/dotfiles";
+  private-secrets = "${config.home.homeDirectory}/private_secrets";
+
   spacemacs = builtins.fetchGit {
     name = "spacemacs";
     url = "git://github.com/syl20bnr/spacemacs";
@@ -27,4 +29,6 @@ in {
     source = "${dotfiles}/sbt";
     recursive = true;
   };
+
+  home.file.".sbt/1.0/sonatype.sbt".source = "${private-secrets}/dotfiles/sonatype.sbt";
 }
