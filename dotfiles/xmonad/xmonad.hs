e4c6e473bb430565b759eb1d412d05cd848070b4
Ian Macalinao <me@ian.pw>
2019-03-31 11:54:42 -0700

    chromium binding

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,3 +1,16 @@
 import XMonad
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig
 
-main = xmonad defaultConfig
+main = xmonad $ defaultConfig {
+          terminal = "urxvt"
+        }
+        `additionalKeys`
+        [
+          ((mod1Mask, xK_BackSpace), spawn "chromium")
+        ]
+        `additionalKeysP`
+        [
+          ("M-x", spawn "rofi -show run")
+        , ("M-z", spawn "rofi -show window")
+        ]
