3e298ebf3db3fc2a807d04abd5871c323c507295
Ian Macalinao <me@ian.pw>
2019-04-14 15:26:45 -0700

    Put swap device in nixos config

--- nixos/machines/ian-nixdesktop.nix
+++ nixos/machines/ian-nixdesktop.nix
@@ -25,7 +25,9 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/04701011-2d17-4b5e-b29f-ed7d52f10a81"; }
+  ];
 
   nix.maxJobs = lib.mkDefault 8;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
