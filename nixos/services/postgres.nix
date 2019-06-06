32e775e3901c6aa2ddeb35200f794fa097bec793
Ian Macalinao <me@ian.pw>
2019-06-06 15:55:11 -0700

    pg 11, make local postgres more trusting

--- nixos/services/postgres.nix
+++ nixos/services/postgres.nix
@@ -3,11 +3,12 @@
 {
   services.postgresql = {
     enable = true;
-    package = pkgs.postgresql;
+    package = pkgs.postgresql_11;
     enableTCPIP = true;
     authentication = pkgs.lib.mkOverride 10 ''
       local all all trust
       host all all 127.0.0.1/32 trust
+      host all all 10.0.0.0/24 trust
     '';
     initialScript = pkgs.writeText "backend-initScript" ''
       CREATE ROLE abacus WITH LOGIN PASSWORD 'abacus' CREATEDB;
