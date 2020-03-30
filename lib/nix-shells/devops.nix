607d43669c94a86f725454d1e2880057ad9b0f07
Ian Macalinao <me@ian.pw>
2020-03-30 03:24:05 -0500

    fix tabs

--- lib/nix-shells/devops.nix
+++ lib/nix-shells/devops.nix
@@ -2,16 +2,16 @@ with import <nixpkgs> {}; {
   devEnv = stdenv.mkDerivation {
     name = "devops";
     buildInputs = [
-        aws-iam-authenticator
-        awscli
-        jx
-        kops
-        kubernetes
-        kubernetes-helm
-        skaffold
-        sops
-        terraform
-        terraform-providers.aws
+      aws-iam-authenticator
+      awscli
+      jx
+      kops
+      kubernetes
+      kubernetes-helm
+      skaffold
+      sops
+      terraform
+      terraform-providers.aws
     ];
   };
 }
