c58abb4f440a614773f2f602fdb68019882b5e83
Ian Macalinao <me@ian.pw>
2020-06-02 18:59:05 -0500

    Fix vscode extensions source (caused by https://github.com/rycee/home-manager/pull/1100)

--- nixhome/programs/vscode.nix
+++ nixhome/programs/vscode.nix
@@ -1,9 +1,207 @@
 { pkgs, lib, ... }:
 
-{
+with lib;
+
+let
+  extensions = with pkgs.vscode-extensions; pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    # Get these hashes by putting in the wrong hash.
+    # `home-manager switch` will tell you what the correct hash is.
+    {
+      name = "vscode-docker";
+      publisher = "ms-azuretools";
+      version = "0.7.0";
+      sha256 = "0bgs6dy429m5yn10dd8m321slf5mqgsbr86ip61kvjwh67q9glcr";
+    }
+    {
+      name = "gitlens";
+      publisher = "eamodio";
+      version = "9.1.0";
+      sha256 = "0a6iqnqmig0s4d107vzwygybndd9hq99kk6mykc88c8qgwf0zdrr";
+    }
+    {
+      name = "vscode-proto3";
+      publisher = "zxh404";
+      version = "0.2.2";
+      sha256 = "1gasx3ay31dy663fcnhpvbys5p7xjvv30skpsqajyi9x2j04akaq";
+    }
+    {
+      name = "vim";
+      publisher = "vscodevim";
+      version = "1.14.5";
+      sha256 = "013h12qhj1h1pcjns2l5dn2hb52p8j468f5n5sw62gq4vfr9yvyf";
+    }
+    {
+      name = "vsc-material-theme";
+      publisher = "equinusocio";
+      version = "2.9.0";
+      sha256 = "1blz6fh60bqny4fskln1a3n0xggfn9w9vdrabh5h73ighqi2w51z";
+    }
+    {
+      name = "prettier-vscode";
+      publisher = "esbenp";
+      version = "1.8.1";
+      sha256 = "0qcm2784n9qc4p77my1kwqrswpji7bp895ay17yzs5g84cj010ln";
+    }
+    {
+      name = "vscode-typescript-tslint-plugin";
+      publisher = "ms-vscode";
+      version = "0.4.1";
+      sha256 = "0fsf9ycc7b09adifipylx1gfg51nmzlqb8n2v3l3g52lx9lxk3is";
+    }
+    {
+      name = "graphql-for-vscode";
+      publisher = "kumar-harsh";
+      version = "1.3.0";
+      sha256 = "0ff0f6g0gq4ckvs9qpkcskz1af9v82xxakzs4rljw85vw8yfpq73";
+    }
+    {
+      name = "vscode-styled-components";
+      publisher = "jpoissonnier";
+      version = "0.0.25";
+      sha256 = "12qgx56g79snkf9r7sgmx3lv0gnzp7avf3a5910i0xq9shfr67n0";
+    }
+    {
+      name = "metals";
+      publisher = "scalameta";
+      version = "1.3.1";
+      sha256 = "1sfpsp8m24k9mmaq1dscpy25mn9f7a9qgsr7sz8flv9b0blb0jcy";
+    }
+    {
+      name = "scala";
+      publisher = "scala-lang";
+      version = "0.2.0";
+      sha256 = "0z2knfgn1g5rvanssnz6ym8zqyzzk5naaqsggrv77k6jzd5lpw49";
+    }
+    {
+      name = "go";
+      publisher = "ms-vscode";
+      version = "0.14.3";
+      sha256 = "0kyq8jp08i8f2hvarjslqynq8i0pyg1w0d25719v4155sjkvd41j";
+    }
+    {
+      name = "terraform";
+      publisher = "mauve";
+      version = "1.3.11";
+      sha256 = "0di7psqcn7gmdl604cxra2xnc8rc6izandqz44qrgjl3j41vp8jr";
+    }
+    {
+      name = "vscode-apollo";
+      publisher = "apollographql";
+      version = "1.16.0";
+      sha256 = "1y4n5kl3ar0dilzal6bmd2ka145x9dvzy2asg0f1bqg1bl8p4kgk";
+    }
+    {
+      name = "language-haskell";
+      publisher = "justusadam";
+      version = "2.6.0";
+      sha256 = "1891pg4x5qkh151pylvn93c4plqw6vgasa4g40jbma5xzq8pygr4";
+    }
+    {
+      name = "vscode-hie-server";
+      publisher = "alanz";
+      version = "0.0.27";
+      sha256 = "1mz0h5zd295i73hbji9ivla8hx02i4yhqcv6l4r23w3f07ql3i8h";
+    }
+    {
+      name = "shell-format";
+      publisher = "foxundermoon";
+      version = "6.0.1";
+      sha256 = "1zkvrlhmw8id65km9cfpgv8p3w1ym4g4mr7cmb32fn3yk937gpmy";
+    }
+    {
+      name = "vscode-github";
+      publisher = "knisterpeter";
+      version = "0.30.2";
+      sha256 = "0axq6a8lgf17kwmsw3fj5g4n0wgwr7x6qfxshaqbl6ac6p1pnd9v";
+    }
+    {
+      name = "vscode-pull-request-github";
+      publisher = "github";
+      version = "0.8.0";
+      sha256 = "0gk9jb8i894jx7a0wjx3w220kh55gyczrfi01b3dcdnwi8gvh80n";
+    }
+    {
+      name = "vscode-spotify";
+      publisher = "shyykoserhiy";
+      version = "3.1.0";
+      sha256 = "1zf09678g16zyqccplhzhv8ciq7wmrz25i2ghw7q0cggm4713g12";
+    }
+    {
+      name = "indent-rainbow";
+      publisher = "oderwat";
+      version = "7.4.0";
+      sha256 = "1xnsdwrcx24vlbpd2igjaqlk3ck5d6jzcfmxaisrgk7sac1aa81p";
+    }
+    {
+      name = "rainbow-brackets";
+      publisher = "2gua";
+      version = "0.0.6";
+      sha256 = "1m5c7jjxphawh7dmbzmrwf60dz4swn8c31svbzb5nhaazqbnyl2d";
+    }
+    {
+      name = "vscode-import-cost";
+      publisher = "wix";
+      version = "2.12.0";
+      sha256 = "1g6k8fxfa49ky8v3l5n6l7p6gnjf9sdd56crcj33p08gb8pyy86l";
+    }
+    {
+      name = "rust";
+      publisher = "rust-lang";
+      version = "0.6.3";
+      sha256 = "1r5q1iclr64wmgglsr3na3sv0fha5di8xyccv7xwcv5jf8w5rz5y";
+    }
+    {
+      name = "vetur";
+      publisher = "octref";
+      version = "0.22.6";
+      sha256 = "0k9akddgbbrjsdl08w4a2nbsj0gi7bki7yjap2c4c1ic9v0lxihi";
+    }
+    {
+      name = "vscode-eslint";
+      publisher = "dbaeumer";
+      version = "1.9.1";
+      sha256 = "0q3vnqmik1228zsy7favzsr3xpaci3z1zh38m79cpy5rac5bcr62";
+    }
+    {
+      name = "better-toml";
+      publisher = "bungcip";
+      version = "0.3.2";
+      sha256 = "08lhzhrn6p0xwi0hcyp6lj9bvpfj87vr99klzsiy8ji7621dzql3";
+    }
+    {
+      name = "prettier-toml";
+      publisher = "bodil";
+      version = "0.1.0";
+      sha256 = "04zyxf4lwkphci6j7svzirha86dad86m3kk7r29skaph2dkw12vg";
+    }
+    {
+      name = "sqltools";
+      publisher = "mtxr";
+      version = "0.21.6";
+      sha256 = "0iyxmj29p6ymnvjwraxxh883gm3asn25azbg1v6dqam700bjlgr2";
+    }
+    {
+      name = "vscode-sql-formatter";
+      publisher = "adpyke";
+      version = "1.4.4";
+      sha256 = "06q78hnq76mdkhsfpym2w23wg9wcpikpfgz07mxk1vnm9h3jm2l3";
+    }
+    {
+      name = "tabnine-vscode";
+      publisher = "TabNine";
+      version = "2.8.3";
+      sha256 = "04zzkfgb4y8dvp83i2h3mf6fpk3kkavzpgz8za622wnl6g0gqlvr";
+    }
+    # {
+    #   name = "vsliveshare";
+    #   publisher = "ms-vsliveshare";
+    #   version = "1.0.2169";
+    #   sha256 = "05j6z2knpzj52fcxzjhakszpm3a5ivjlnp9x7g9vjpmzk2w6zj15";
+    # }
+  ] ++ [ bbenoist.Nix ];
+in {
   programs.vscode = {
     enable = true;
-    haskell.hie.enable = true;
     userSettings = {
       "editor.defaultFormatter" = "esbenp.prettier-vscode";
       "editor.formatOnSave" = true;
@@ -65,203 +263,24 @@
         "source.fixAll.eslint" = true;
       };
     };
-    extensions = with pkgs.vscode-extensions; pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      # Get these hashes by putting in the wrong hash.
-      # `home-manager switch` will tell you what the correct hash is.
-      {
-        name = "vscode-docker";
-        publisher = "ms-azuretools";
-        version = "0.7.0";
-        sha256 = "0bgs6dy429m5yn10dd8m321slf5mqgsbr86ip61kvjwh67q9glcr";
-      }
-      {
-        name = "gitlens";
-        publisher = "eamodio";
-        version = "9.1.0";
-        sha256 = "0a6iqnqmig0s4d107vzwygybndd9hq99kk6mykc88c8qgwf0zdrr";
-      }
-      {
-        name = "vscode-proto3";
-        publisher = "zxh404";
-        version = "0.2.2";
-        sha256 = "1gasx3ay31dy663fcnhpvbys5p7xjvv30skpsqajyi9x2j04akaq";
-      }
-      {
-        name = "vim";
-        publisher = "vscodevim";
-        version = "1.14.5";
-        sha256 = "013h12qhj1h1pcjns2l5dn2hb52p8j468f5n5sw62gq4vfr9yvyf";
-      }
-      {
-        name = "vsc-material-theme";
-        publisher = "equinusocio";
-        version = "2.9.0";
-        sha256 = "1blz6fh60bqny4fskln1a3n0xggfn9w9vdrabh5h73ighqi2w51z";
-      }
-      {
-        name = "prettier-vscode";
-        publisher = "esbenp";
-        version = "1.8.1";
-        sha256 = "0qcm2784n9qc4p77my1kwqrswpji7bp895ay17yzs5g84cj010ln";
-      }
-      {
-        name = "vscode-typescript-tslint-plugin";
-        publisher = "ms-vscode";
-        version = "0.4.1";
-        sha256 = "0fsf9ycc7b09adifipylx1gfg51nmzlqb8n2v3l3g52lx9lxk3is";
-      }
-      {
-        name = "graphql-for-vscode";
-        publisher = "kumar-harsh";
-        version = "1.3.0";
-        sha256 = "0ff0f6g0gq4ckvs9qpkcskz1af9v82xxakzs4rljw85vw8yfpq73";
-      }
-      {
-        name = "vscode-styled-components";
-        publisher = "jpoissonnier";
-        version = "0.0.25";
-        sha256 = "12qgx56g79snkf9r7sgmx3lv0gnzp7avf3a5910i0xq9shfr67n0";
-      }
-      {
-        name = "metals";
-        publisher = "scalameta";
-        version = "1.3.1";
-        sha256 = "1sfpsp8m24k9mmaq1dscpy25mn9f7a9qgsr7sz8flv9b0blb0jcy";
-      }
-      {
-        name = "scala";
-        publisher = "scala-lang";
-        version = "0.2.0";
-        sha256 = "0z2knfgn1g5rvanssnz6ym8zqyzzk5naaqsggrv77k6jzd5lpw49";
-      }
-      {
-        name = "go";
-        publisher = "ms-vscode";
-        version = "0.14.3";
-        sha256 = "0kyq8jp08i8f2hvarjslqynq8i0pyg1w0d25719v4155sjkvd41j";
-      }
-      {
-        name = "terraform";
-        publisher = "mauve";
-        version = "1.3.11";
-        sha256 = "0di7psqcn7gmdl604cxra2xnc8rc6izandqz44qrgjl3j41vp8jr";
-      }
-      {
-        name = "vscode-apollo";
-        publisher = "apollographql";
-        version = "1.16.0";
-        sha256 = "1y4n5kl3ar0dilzal6bmd2ka145x9dvzy2asg0f1bqg1bl8p4kgk";
-      }
-      {
-        name = "language-haskell";
-        publisher = "justusadam";
-        version = "2.6.0";
-        sha256 = "1891pg4x5qkh151pylvn93c4plqw6vgasa4g40jbma5xzq8pygr4";
-      }
-      {
-        name = "vscode-hie-server";
-        publisher = "alanz";
-        version = "0.0.27";
-        sha256 = "1mz0h5zd295i73hbji9ivla8hx02i4yhqcv6l4r23w3f07ql3i8h";
-      }
-      {
-        name = "shell-format";
-        publisher = "foxundermoon";
-        version = "6.0.1";
-        sha256 = "1zkvrlhmw8id65km9cfpgv8p3w1ym4g4mr7cmb32fn3yk937gpmy";
-      }
-      {
-        name = "vscode-github";
-        publisher = "knisterpeter";
-        version = "0.30.2";
-        sha256 = "0axq6a8lgf17kwmsw3fj5g4n0wgwr7x6qfxshaqbl6ac6p1pnd9v";
-      }
-      {
-        name = "vscode-pull-request-github";
-        publisher = "github";
-        version = "0.8.0";
-        sha256 = "0gk9jb8i894jx7a0wjx3w220kh55gyczrfi01b3dcdnwi8gvh80n";
-      }
-      {
-        name = "vscode-spotify";
-        publisher = "shyykoserhiy";
-        version = "3.1.0";
-        sha256 = "1zf09678g16zyqccplhzhv8ciq7wmrz25i2ghw7q0cggm4713g12";
-      }
-      {
-        name = "indent-rainbow";
-        publisher = "oderwat";
-        version = "7.4.0";
-        sha256 = "1xnsdwrcx24vlbpd2igjaqlk3ck5d6jzcfmxaisrgk7sac1aa81p";
-      }
-      {
-        name = "rainbow-brackets";
-        publisher = "2gua";
-        version = "0.0.6";
-        sha256 = "1m5c7jjxphawh7dmbzmrwf60dz4swn8c31svbzb5nhaazqbnyl2d";
-      }
-      {
-        name = "vscode-import-cost";
-        publisher = "wix";
-        version = "2.12.0";
-        sha256 = "1g6k8fxfa49ky8v3l5n6l7p6gnjf9sdd56crcj33p08gb8pyy86l";
-      }
-      {
-        name = "rust";
-        publisher = "rust-lang";
-        version = "0.6.3";
-        sha256 = "1r5q1iclr64wmgglsr3na3sv0fha5di8xyccv7xwcv5jf8w5rz5y";
-      }
-      {
-        name = "vetur";
-        publisher = "octref";
-        version = "0.22.6";
-        sha256 = "0k9akddgbbrjsdl08w4a2nbsj0gi7bki7yjap2c4c1ic9v0lxihi";
-      }
-      {
-        name = "vscode-eslint";
-        publisher = "dbaeumer";
-        version = "1.9.1";
-        sha256 = "0q3vnqmik1228zsy7favzsr3xpaci3z1zh38m79cpy5rac5bcr62";
-      }
-      {
-        name = "better-toml";
-        publisher = "bungcip";
-        version = "0.3.2";
-        sha256 = "08lhzhrn6p0xwi0hcyp6lj9bvpfj87vr99klzsiy8ji7621dzql3";
-      }
-      {
-        name = "prettier-toml";
-        publisher = "bodil";
-        version = "0.1.0";
-        sha256 = "04zyxf4lwkphci6j7svzirha86dad86m3kk7r29skaph2dkw12vg";
-      }
-      {
-        name = "sqltools";
-        publisher = "mtxr";
-        version = "0.21.6";
-        sha256 = "0iyxmj29p6ymnvjwraxxh883gm3asn25azbg1v6dqam700bjlgr2";
-      }
-      {
-        name = "vscode-sql-formatter";
-        publisher = "adpyke";
-        version = "1.4.4";
-        sha256 = "06q78hnq76mdkhsfpym2w23wg9wcpikpfgz07mxk1vnm9h3jm2l3";
-      }
-      {
-        name = "tabnine-vscode";
-        publisher = "TabNine";
-        version = "2.8.3";
-        sha256 = "04zzkfgb4y8dvp83i2h3mf6fpk3kkavzpgz8za622wnl6g0gqlvr";
-      }
-      # {
-      #   name = "vsliveshare";
-      #   publisher = "ms-vsliveshare";
-      #   version = "1.0.2169";
-      #   sha256 = "05j6z2knpzj52fcxzjhakszpm3a5ivjlnp9x7g9vjpmzk2w6zj15";
-      # }
-    ] ++ [
-      bbenoist.Nix
-    ];
   };
+
+  # Adapted from https://discourse.nixos.org/t/vscode-extensions-setup/1801/2
+  home.file =
+    let
+      toPaths = path:
+        let
+          p = "${path}/share/vscode/extensions";
+        in
+          # Links every dir in p to the extension path.
+          mapAttrsToList (k: v:
+            {
+              ".vscode/extensions/${k}".source = "${p}/${k}";
+            }) (builtins.readDir p);
+      toSymlink = concatMap toPaths extensions;
+    in
+      foldr
+        (a: b: a // b)
+        { }
+        toSymlink;
 }
