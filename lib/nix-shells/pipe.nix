db296baa3fa893075066664f49832475260d96b5
Ian Macalinao <me@ian.pw>
2020-09-22 19:58:26 -0700

    pipe nix shell

--- lib/nix-shells/pipe.nix
+++ lib/nix-shells/pipe.nix
@@ -1,21 +1,47 @@
-with import <nixpkgs> {}; {
-  devEnv = stdenv.mkDerivation {
+# This shell contains several useful utilities for interacting with the Pipe codebase.
+with import <nixpkgs> {};
+let
+  buildInputs = [
+    # frontend
+    yarn
+    nodejs-12_x
+
+    # backend
+    go
+    goreman
+    stdenv
+    zsh
+    pg_flame
+    postgresql_12
+
+    # Golang tools
+    delve
+    go-outline
+    go-symbols
+    gocode
+    gocode-gomod
+    godef
+    goimports
+    golangci-lint
+    gopkgs
+    gopls
+
+    # devops
+    heroku
+
+    # dev
+    docker-compose
+  ];
+  drvArgs = if stdenv.isDarwin then {
     name = "dev";
-    buildInputs = [
-        yarn
-        nodejs-12_x
-
-        go
-        go-outline
-        goreman
-        stdenv
-        zsh
-
-        darwin.apple_sdk.frameworks.CoreFoundation
-        pg_flame
-        postgresql_12
+    buildInputs = buildInputs ++ [
+      # osx stuff
+      darwin.apple_sdk.frameworks.CoreFoundation
     ];
     CFLAGS="-I/usr/include";
     LDFLAGS="-L${darwin.apple_sdk.frameworks.CoreFoundation}/Library/Frameworks";
+  } else {
+    name = "dev";
+    buildInputs = buildInputs;
   };
-}
+in stdenv.mkDerivation drvArgs
