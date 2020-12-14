c63a62e4ce53c4e3bd2de35058e820c857355b64
Ian Macalinao <me@ian.pw>
2020-12-14 15:43:02 -0800

    nixfmt

--- nix/darwin/default.nix
+++ nix/darwin/default.nix
@@ -1,16 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages =
-    [
-      pkgs.vim
-    ];
+  environment.systemPackages = [ pkgs.vim ];
 
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
 
-  programs.bash.enable = true;  # default shell on catalina
-  programs.zsh.enable = true;  # default shell on catalina
+  programs.bash.enable = true; # default shell on catalina
+  programs.zsh.enable = true; # default shell on catalina
 
   services.yabai = {
     enable = true;
