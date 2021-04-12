5b39229d724ef6481f370a209f9182ec72fff3ba
Ian Macalinao <github@igm.pub>
2021-04-12 14:48:34 -0500

    Cleanup openvpn code

--- nix/nixos/services/pia.nix
+++ nix/nixos/services/pia.nix
@@ -1,6 +1,6 @@
 # from https://github.com/illegalprime/nix/blob/1eb90ceaa9af14eba9d10d1178076e428994de0d/nixos/pia-system.nix
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 with builtins;
 
@@ -10,7 +10,7 @@ with builtins;
   # Configure all our servers
   # Use with `sudo systemctl start openvpn-us-east`
   services.openvpn.servers = let
-    vpn_str = with (import <nixpkgs> { }).lib.strings;
+    vpn_str = with lib.strings;
       file:
       removeSuffix ".ovpn" (toLower (replaceStrings [ " " ] [ "-" ] file));
   in foldl' (init: file:
