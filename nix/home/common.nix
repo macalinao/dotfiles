4b3d35e459604d055b29acbda64792bf726d10db
Ian Macalinao <github@igm.pub>
2021-07-02 14:02:30 -0400

    solana

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -128,6 +128,11 @@
       fi
       . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
       export PATH="$HOME/.cargo/bin:$PATH"
+      ${
+      # Solana path on Darwin
+      lib.optionalString pkgs.stdenv.isDarwin ''
+        export PATH="/Users/igm/.local/share/solana/install/active_release/bin:$PATH"
+      ''}
 
       setopt appendhistory
       setopt INC_APPEND_HISTORY  
