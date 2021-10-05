36261f14d82c79b486147e7f1e07d1ad00ae0e89
Ian Macalinao <github@igm.pub>
2021-10-05 14:31:41 -0500

    update flakes again

--- nix/nixpkgs/overlays/1-basic.nix
+++ nix/nixpkgs/overlays/1-basic.nix
@@ -14,4 +14,13 @@ self: super: rec {
       sha256 = "sha256-re3pVOnGltluJUdZtTlSeiSrHULw1UjFxDCdGj/Dwl4=";
     };
   };
+  python39 = super.python39.override {
+    packageOverrides = self: super: {
+      beautifulsoup4 = super.beautifulsoup4.overrideAttrs (old: {
+        propagatedBuildInputs =
+          super.lib.remove super.lxml old.propagatedBuildInputs;
+      });
+    };
+  };
+  python39Packages = python39.pkgs;
 }
