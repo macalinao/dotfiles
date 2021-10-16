77ee8a2e5be79e4922706278dac8a1fab8909026
Ian Macalinao <github@igm.pub>
2021-10-16 01:01:30 -0700

    update vscode

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,7 +2,7 @@
   description = "Ian's dotfiles.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
