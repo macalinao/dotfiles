8ff2879efe76b54eb005c099b8ab25521d9c8583
Ian Macalinao <github@igm.pub>
2022-03-05 18:23:25 -0600

    more keys

--- nix/nixos/users.nix
+++ nix/nixos/users.nix
@@ -10,7 +10,8 @@
         shell = pkgs.zsh;
         isNormalUser = true;
         extraGroups = [ "wheel" "docker" "transmission" ];
-        openssh.authorizedKeys.keyFiles = [ ./authorized-keys/ian-mbp.pub ];
+        openssh.authorizedKeys.keyFiles =
+          [ ./authorized-keys/ian-mbp.pub ./authorized-keys/ian-mbp-2022.pub ];
       };
     };
   };
