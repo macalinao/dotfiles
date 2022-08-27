eb7abfe5df8ba23fd2f0db5821e715979b56ed6b
Ian Macalinao <github@igm.pub>
2022-08-27 08:16:53 -0500

    remove unused kitty stuff

--- nix/nixpkgs/overlays/10-basic.nix
+++ nix/nixpkgs/overlays/10-basic.nix
@@ -5,18 +5,4 @@ self: super: rec {
     rev = "0dbd119465ff2ccbe43cb83431eba792b536a640";
     sha256 = "1zxgy3znw0i6h1lxhmnx001c1pdcyszwqj8f0d0092nmnngdzsrl";
   };
-
-  # tests for this are broken on darwin
-  python3Packages = super.python3Packages // {
-    pycurl = super.python3Packages.pycurl.overrideAttrs
-      (oldAttrs: {
-        disabledTests = oldAttrs.disabledTests ++ [
-          # pycurl.error: (27, '')
-          "test_getinfo_raw_certinfo"
-          "test_request_with_certinfo"
-          "test_request_with_verifypeer"
-          "test_request_without_certinfo"
-        ];
-      });
-  };
 }
