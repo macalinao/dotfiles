6cab9e6ce9688f213e71209f711bc5dbaababd65
Ian Macalinao <github@igm.pub>
2023-06-13 12:27:44 +0900

    fix fcitx5 again

--- nix/nixos/gui.nix
+++ nix/nixos/gui.nix
@@ -14,12 +14,13 @@
     proggyfonts
   ];
 
-  # This enables "fcitx" as your IME.  This is an easy-to-use IME.  It supports many different input methods.
-  i18n.inputMethod.enabled = "fcitx5";
-
-  # This enables "mozc" as an input method in "fcitx".  This has a relatively
-  # complete dictionary.  I recommend it for Japanese input.
-  i18n.inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ mozc hangul ];
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-hangul
+    ];
+  };
 
   # Enable sound.
   sound.enable = true;
