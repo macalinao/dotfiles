dfe8f9d8362e60c8b4c2472cf5e3ee0527be4c02
Ian Macalinao <github@igm.pub>
2023-09-18 12:13:54 +0800

    yarn sdks alias

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -185,6 +185,7 @@
       localip =
         "ifconfig | grep -Eo 'inet (addr:)?([0-9]*.){3}[0-9]*' | grep -Eo '([0-9]*.){3}[0-9]*' | grep -v '127.0.0.1'";
       funky = "sfxl fortnite";
+      ysdk = "yarn dlx @yarnpkg/sdks vscode";
     };
 
     history = {
