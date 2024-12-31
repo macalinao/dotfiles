f3c98f55092afe591bd1d526e3fcb5e953c26728
Ian Macalinao <github@igm.pub>
2024-12-31 16:50:58 +0800

    Updates

--- flake.nix
+++ flake.nix
@@ -4,17 +4,28 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
-    { flake-utils, nixpkgs, ... }:
+    {
+      flake-utils,
+      nixpkgs,
+      treefmt-nix,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.nixfmt-rfc-style.enable = true;
+          programs.shfmt.enable = true;
+        };
       in
       {
-        formatter = pkgs.nixfmt-rfc-style;
+        formatter = treefmtEval.config.build.wrapper;
         devShells.default = import ./nix/shells/nix.nix { inherit pkgs; };
       }
     );
