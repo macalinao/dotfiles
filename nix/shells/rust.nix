9328346398ae2347b9048bb536bfc76ddb368041
Ian Macalinao <github@igm.pub>
2025-10-25 17:19:31 +0800

    fix rust shell

--- nix/shells/rust.nix
+++ nix/shells/rust.nix
@@ -2,23 +2,19 @@
 with pkgs;
 mkShell {
   name = "devshell-rust";
-  buildInputs =
-    [
-      rustup
-      pkg-config
-      openssl
-      cargo-hakari
-      cargo-outdated
-      cargo-readme
-      cargo-workspaces
-    ]
-    ++ (lib.optional stdenv.isDarwin (
-      [ libiconv ]
-      ++ (with darwin.apple_sdk.frameworks; [
-        DiskArbitration
-        Foundation
-      ])
-    ));
+  buildInputs = [
+    rustup
+    pkg-config
+    openssl
+    cargo-hakari
+    cargo-outdated
+    cargo-readme
+    cargo-workspaces
+  ]
+  ++ (lib.optional stdenv.isDarwin ([
+    libiconv
+    apple-sdk
+  ]));
 
   packages = [
     cargo-expand
