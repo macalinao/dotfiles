1f593c392c2a0de5fb18268d6ae410dc84b1a97b
Ian Macalinao <github@igm.pub>
2025-11-15 10:52:17 +0800

    difftastic fixes

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -131,8 +131,12 @@
     };
   };
 
-  programs.difftastic.git = {
-    diffToolMode = true;
+  programs.difftastic = {
+    enable = true;
+    git = {
+      enable = true;
+      diffToolMode = true;
+    };
   };
 
   programs.delta = {
