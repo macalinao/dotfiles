6027b35e76d164de68f933e0d2fb9d833f124817
Ian Macalinao <github@igm.pub>
2026-04-13 13:09:18 +0800

    Revert nixpkgs config from homeModules
    nixpkgs options in homeModules conflict with useGlobalPkgs in NixOS.
    Consumers that need allowUnfree/overlays should set them themselves.

--- nix/modules/home-manager.nix
+++ nix/modules/home-manager.nix
@@ -1,25 +1,9 @@
-{ inputs, ... }:
-let
-  nixpkgsConfig = {
-    nixpkgs.config.allowUnfree = true;
-    nixpkgs.overlays = [ inputs.self.overlays.default ];
-  };
-in
+{ ... }:
 {
   flake = {
     homeModules = {
-      default = {
-        imports = [
-          ../home/modules
-          nixpkgsConfig
-        ];
-      };
-      headless = {
-        imports = [
-          ../home/modules/headless.nix
-          nixpkgsConfig
-        ];
-      };
+      default = import ../home/modules;
+      headless = import ../home/modules/headless.nix;
     };
   };
 }
