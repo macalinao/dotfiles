5e87f038a7e4f85e09fc74e675ceb9892cebfa22
Ian Macalinao <github@igm.pub>
2026-04-13 12:58:17 +0800

    Bundle nixpkgs config (allowUnfree, overlays) into homeModules
    Standalone home-manager consumers no longer need to configure
    allowUnfree or the dotfiles overlay themselves.

--- nix/modules/home-manager.nix
+++ nix/modules/home-manager.nix
@@ -1,9 +1,25 @@
-{ ... }:
+{ inputs, ... }:
+let
+  nixpkgsConfig = {
+    nixpkgs.config.allowUnfree = true;
+    nixpkgs.overlays = [ inputs.self.overlays.default ];
+  };
+in
 {
   flake = {
     homeModules = {
-      default = import ../home/modules;
-      headless = import ../home/modules/headless.nix;
+      default = {
+        imports = [
+          ../home/modules
+          nixpkgsConfig
+        ];
+      };
+      headless = {
+        imports = [
+          ../home/modules/headless.nix
+          nixpkgsConfig
+        ];
+      };
     };
   };
 }
